19f722cb71734bf9f19df1a0c1ffaecc650de225
LeoooooGadil <leogadil04@gmail.com>
2023-03-26 23:15:19 +0800

    i did something

--- Assets/Scripts/Statistics/Experience/ExperienceManager.cs
+++ Assets/Scripts/Statistics/Experience/ExperienceManager.cs
@@ -2,20 +2,47 @@ using UnityEngine;
 
 public class ExperienceManager : MonoBehaviour
 {
+	public static ExperienceManager instance;
 	private ExperienceSave experienceSave;
 	private string saveFileName = "experience";
 
+	void Awake() {
+		if (instance != null)
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
 	// this method is called when the player gains experience
-	// add a check to see if the player experience (experience) is greater than the experience to next level (experienceToNextLevelMultiplier)
+	// add a check to see if the player experience (experience) is greater than the experience to next level (experienceToNextLevel)
 	// call SaveExperience() to save the experience
+	// reset the the experience but keep the remainder (experience)
 	public void AddExperience(float xp)
 	{
-		throw new System.NotImplementedException();
+		float exp = experienceSave.GetExperience();
+		float expToNextLevel = experienceSave.GetExperienceToNextLevel();
+
+		float newExp = exp + xp;
+
+		if (newExp >= expToNextLevel)
+		{
+			newExp = expToNextLevel - newExp;
+			AdvanceLevel();
+			experienceSave.SetExperience(newExp);
+		}
+		else
+			experienceSave.SetExperience(newExp);
+
+		SaveExperience();
 	}
 
 	// this method is called when the player levels up
 	// increase the level (level) by 1
-	// reset the the experience but keep the remainder (experience)
 	// call SaveExperience() to save the experience
 	public void AdvanceLevel()
 	{
