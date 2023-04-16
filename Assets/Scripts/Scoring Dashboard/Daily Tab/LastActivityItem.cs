2c90cfea6c53d4833e86930cf2bd14ecc5d6a29c
OlympianDog <kyleolympian30@gmail.com>
2023-04-16 19:25:54 +0800

    typo error with the code

--- Assets/Scripts/Scoring Dashboard/Daily Tab/LastActivityItem.cs
+++ Assets/Scripts/Scoring Dashboard/Daily Tab/LastActivityItem.cs
@@ -40,12 +40,12 @@ public class LastActivityItem : MonoBehaviour
 		else if (timeDiff.TotalHours < 1)
 		{
 			int minutesAgo = (int)Math.Round(timeDiff.TotalMinutes);
-			activityTimestampText.text = timeDiff.minutesAgo + " minutes" + (minutesAgo != 1 ? "s" : "") + " ago";
+			activityTimestampText.text = minutesAgo + " minutes" + (minutesAgo != 1 ? "s" : "") + " ago";
 		}
 		else if (timeDiff.TotalDays < 1)
 		{
 			int hoursAgo = (int)Math.Round(timeDiff.TotalHours);
-			activityTimestampText.text = timeDiff.hoursAgo + " hours" + (hoursAgo != 1 ? "s" : "") + " ago";
+			activityTimestampText.text = hoursAgo + " hours" + (hoursAgo != 1 ? "s" : "") + " ago";
 		}
 		else
 		{
